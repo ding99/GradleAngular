b6c42f30313583312b857731610a38634b25d1d7
Wei Ding <ding_web@yahoo.co.jp>
2021-06-20 14:21:29 -0400

    Initial commit

---
+++ fe-service/src/main/java/combine/first/feservice/FeServiceApplication.java
@@ -0,0 +1,13 @@
+package combine.first.feservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeServiceApplication.class, args);
+	}
+
+}
---
+++ fe-service/src/test/java/combine/first/feservice/FeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package combine.first.feservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
